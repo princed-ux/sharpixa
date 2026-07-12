329196213c56f7c694a0098d995ef5fd8b895f48
princed-ux <koladetheprince@gmail.com>
2026-07-12 17:44:26 +0100

    commit 8903456878

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,6 +111,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-3890435207453108" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3890435207453108"
